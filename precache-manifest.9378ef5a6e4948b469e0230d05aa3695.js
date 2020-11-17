c5b3c84af264d09050db7adde72a2c42d61d227f
kcee403 <kcee403@gmail.com>
2020-11-16 21:55:05 -0500

    Updates

---
+++ precache-manifest.9378ef5a6e4948b469e0230d05aa3695.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07db3477421f84df823868eddde13932",
+    "url": "/bensonbracelets/index.html"
+  },
+  {
+    "revision": "2dbc70a15ac85734e45e",
+    "url": "/bensonbracelets/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "2057d8e28412980c6104",
+    "url": "/bensonbracelets/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2dbc70a15ac85734e45e",
+    "url": "/bensonbracelets/static/js/2.fe0cd7a7.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/bensonbracelets/static/js/2.fe0cd7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2057d8e28412980c6104",
+    "url": "/bensonbracelets/static/js/main.d8f11801.chunk.js"
+  },
+  {
+    "revision": "7b2a7e7f1332a106d7df",
+    "url": "/bensonbracelets/static/js/runtime-main.17ed17b1.js"
+  },
+  {
+    "revision": "26ec906c304a38659f3faa93781d562b",
+    "url": "/bensonbracelets/static/media/bensonbracelet-hero-parallax.26ec906c.jpg"
+  },
+  {
+    "revision": "73e2d958b65f0ec1789823a6714a0b17",
+    "url": "/bensonbracelets/static/media/bracelet1.73e2d958.jpg"
+  },
+  {
+    "revision": "3160abc7f9802dc3410ca5ffb8279bd7",
+    "url": "/bensonbracelets/static/media/bracelet10.3160abc7.jpg"
+  },
+  {
+    "revision": "59f52f6d3f41049cbf5d01ed23dbaeb9",
+    "url": "/bensonbracelets/static/media/bracelet2.59f52f6d.jpg"
+  },
+  {
+    "revision": "827afa3ff9cee907eaf826f8014200ab",
+    "url": "/bensonbracelets/static/media/bracelet3.827afa3f.jpg"
+  },
+  {
+    "revision": "215de3120c059809fb58d548d8f2a259",
+    "url": "/bensonbracelets/static/media/bracelet4.215de312.jpg"
+  },
+  {
+    "revision": "b4291a729934bff1a78e198160e715b0",
+    "url": "/bensonbracelets/static/media/bracelet5.b4291a72.jpg"
+  },
+  {
+    "revision": "8bb52464f32035a1bb1fcbf1644c09fd",
+    "url": "/bensonbracelets/static/media/bracelet6.8bb52464.jpg"
+  },
+  {
+    "revision": "8aea587102cd4ae39e33bfe226ebfbd6",
+    "url": "/bensonbracelets/static/media/bracelet7.8aea5871.jpg"
+  },
+  {
+    "revision": "f370e8771d710ff6da04d7790000475a",
+    "url": "/bensonbracelets/static/media/bracelet8.f370e877.jpg"
+  },
+  {
+    "revision": "07a167d8e0dc2b697c5d6c9f4c8dbc56",
+    "url": "/bensonbracelets/static/media/bracelet9.07a167d8.jpg"
+  },
+  {
+    "revision": "0820522b2f26a10ecfd059a884e3bf6c",
+    "url": "/bensonbracelets/static/media/facebook.0820522b.svg"
+  },
+  {
+    "revision": "21b0da2512113100cbce569fdd3de198",
+    "url": "/bensonbracelets/static/media/fraternity88by1ratio753x856px.21b0da25.jpg"
+  },
+  {
+    "revision": "50122a24fe427cee1dc49137e51bbbad",
+    "url": "/bensonbracelets/static/media/heroImgMobile5.50122a24.jpg"
+  },
+  {
+    "revision": "2f1d65677a716d3ce894ee4eec465908",
+    "url": "/bensonbracelets/static/media/instagram.2f1d6567.svg"
+  },
+  {
+    "revision": "5d0dc2ad0fd38523d5dbee2deca3c9a5",
+    "url": "/bensonbracelets/static/media/luxury88by1ratio.5d0dc2ad.jpg"
+  },
+  {
+    "revision": "4e105b7b58cd780b01d5bfb18cdbec9b",
+    "url": "/bensonbracelets/static/media/teamcolors88by1ratio440x500px.4e105b7b.jpg"
+  }
+]);
